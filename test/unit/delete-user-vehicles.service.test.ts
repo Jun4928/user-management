380924d88e9febd4acbdf281828a87a402f7bb57
Jun <63147802+Jun4928@users.noreply.github.com>
2024-02-10 18:30:06 +0100

    feat: implements delete-user-vehicles and test (#7)

--- test/unit/delete-user-vehicles.service.test.ts
+++ test/unit/delete-user-vehicles.service.test.ts
@@ -3,9 +3,9 @@ import {
   TARGET_NOT_FOUND,
   VehicleDatabase,
 } from "../../src/database/vehicle-database";
-import { updateUserVehicle } from "../../src/service/update-user-vehicle.service";
+import { deleteUserVehicles } from "../../src/service/delete-user-vehicles.service";
 
-describe(`SERVICE: update-user-vehicles test`, () => {
+describe(`SERVICE: delete-user-vehicles test`, () => {
   let vehicleDatabase: VehicleDatabase;
 
   beforeAll(async () => {
@@ -43,49 +43,36 @@ describe(`SERVICE: update-user-vehicles test`, () => {
     );
   });
 
-  test(`update successfully`, async () => {
+  test(`delete successfully`, async () => {
     const input = {
       userId: 1,
       vehicleId: 1,
-      vehicle: {
-        brand: "Volkswagen",
-        model: "beautiful",
-        year: 2022,
-      },
     };
 
-    const result = await updateUserVehicle(
+    await deleteUserVehicles(
       {
         userId: input.userId,
         vehicleId: input.vehicleId,
-        vehicle: input.vehicle,
       },
       vehicleDatabase,
     );
 
-    expect(result.id).toBe(input.vehicleId);
-    expect(result.brand).toBe(input.vehicle.brand);
-    expect(result.model).toBe(input.vehicle.model);
-    expect(result.year).toBe(input.vehicle.year);
+    expect(
+      await vehicleDatabase.getOne(input.userId, input.vehicleId),
+    ).toBeNull();
   });
 
   test(`when there is no target, throw NOT FOUND ERROR`, async () => {
     const input = {
       userId: 2,
       vehicleId: 4,
-      vehicle: {
-        brand: "Volkswagen",
-        model: "beautiful",
-        year: 2022,
-      },
     };
 
     try {
-      await updateUserVehicle(
+      await deleteUserVehicles(
         {
           userId: input.userId,
           vehicleId: input.vehicleId,
-          vehicle: input.vehicle,
         },
         vehicleDatabase,
       );
